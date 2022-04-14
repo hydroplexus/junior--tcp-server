739a51458d888ffdf8a94417b156187ec6f1218f
tesla <shakhov-vy@mail.ru>
2022-04-14 23:50:15 +0700

    1

--- server.py
+++ server.py
@@ -1,40 +1,49 @@
-#!/usr/bin/python
-
 import socketserver
 import re
     
 class Server(socketserver.TCPServer):
     
     class HRecieve(socketserver.BaseRequestHandler):
-        log_dir = 'log/'
-        log_err = 'err.log'
-        log_result = 'result.log'
-        __pattern = re.compile('[0-9]')
-        __response = b''
+        def setup(self):
+            self.__pattern = re.compile(r'(?P<num>\d{4}) (?P<chn>C\d) (?P<at>\d{2}:\d{2}:\d{2}.\d)\d{2} (?P<grp>\d{2})$')
+            self.__response = b''
+            self.client = self.client_address[0] + ':' + str(self.client_address[1])
+            logName = './results.log'
+            self.logFile = open(logName, 'a')
         
+        def finish(self):
+            print('Client ' + self.client + ' disconnected\r\n'
+                  'Waiting for new one...')
+            self.logFile.close()
+            
         def handle(self):
             accum = b''
+            print('Connected client from ' + self.client + '\r\n'
+                  'Start recieving data...')
             while True:
                 data = self.request.recv(1024)
                 if not data: break
                 accum += data
                 if accum.find(b'\r'):
                     self.__response += accum
-                    print (self.__response)
                     accum = b''
                     self.check()
                     
         def check(self):
             list = self.__response.split(b'\r')
-            print(list)
+            for record in list:
+                match = self.__pattern.search(record.decode('ascii'))
+                if match:
+                    self.logger(match)
+            self.__response = b''
             
-        def convert(self, data):
-            pass
-        
-        def logger(self):
-            pass
+        def logger(self, match):
+            if match.group('grp') == '00':
+                confirm = 'Спортсмен, нагрудный номер ' + match.group('num') + ' прошёл отсечку ' + match.group('chn') + ' в "' + match.group('at') + '"'
+                print(confirm)
+            self.logFile.write(match.group(0))
             
-            
+    
     def __init__(self, host='0.0.0.0', port=5000):
         try:
             super().__init__((host, port), self.HRecieve)
