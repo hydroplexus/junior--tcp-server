c8ae90c813ff07941a622d1a51819371261ee8e9
hydroplexus <37233310+hydroplexus@users.noreply.github.com>
2022-04-14 11:49:14 +0700

    2

--- server.py
+++ server.py
@@ -1,18 +1,43 @@
 #!/usr/bin/python
 
-import socket
-import sys
-
-from PySide6.QtCore import QObject
-
-class JuniorServer(QObject):
+import socketserver
     
-    def __init__(self) -> None:
-        super().__init__(parent)
-
-def main(): -> None:
-    server = JuniorServer
-    server.start()
+class JData():
+    def __init__(self, data='')
+        self.__data=data
     
+    def check(data):
+        return True
+        
+    def convert(self):
+        pass
+    
+    
+class JServer(socketserver.TCPServer):
+    
+    class HRecieve(socketserver.BaseRequestHandler):
+        __data = JData()
+        
+        def handle(self):
+            self.__data = self.request.recv(1024).strip()
+            if (self,__data.check)
+                print("Data is alright")
+            
+            
+    __reciever = HRecieve()
+    
+    def __init__(self, host='0.0.0.0', port=5000):
+          super.__init__()
+          self.server_bind((host, port), self.__reciever)
+          
+          
+def main() -> None:
+    host = '0.0.0.0'
+    port = 5000
+    reciever = HRecieve()
+    server = JServer(host, port)
+    server.serve_forever()
+     
+        
 if __name__ == "__main__":
     main()
